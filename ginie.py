421b114e73b9828ca5949a10f0993e53d296a333
SHIN2712 <jeongho2712@gmail.com>
2020-10-29 15:06:18 +0900

    week04_homework_sweet

--- ginie.py
+++ ginie.py
@@ -7,14 +7,12 @@ db = client.dbsparta  # 'dbsparta'라는 이름의 db를 사용합니다. 'dbspa
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# 아래 빈 칸('')을 채워보세요
-data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20181025', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
-# 아래 빈 칸('')을 채워보세요
 for tr in trs:
-    rank = tr.select_one('td.number').text[0:2].strip()
+    rank = tr.select_one('td.number').text[:2].strip()
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
     doc = {
@@ -22,5 +20,5 @@ for tr in trs:
         'title': title,
         'artist': artist
     }
-    db.music_chart.insert_one(doc)
+    # db.music_chart.insert_one(doc)
     print(rank, title, artist)
